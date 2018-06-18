366faa38fb56ecc67baf2d0295f1a00bc53cafc0
Emrys Hughes <>
2018-06-18 17:46:31 +1000

    rweaks

--- src/components/Question.js
+++ src/components/Question.js
@@ -11,15 +11,11 @@ class Question extends Component {
     this.state = {
       question: 1
     }
-    
   }
 
   incrementQuestion = () => {
     this.setState({ question: this.state.question + 1 });
   }
-
-  
-
 
   render() {
   
@@ -32,77 +28,41 @@ class Question extends Component {
       return null;
     }
 
+    // function to create an array from the js object so we can pull out what we need
     const objectToArray = obj => {
-    /*  Object.keys(obj).map(function(key) {
-        return [Number(key), obj[key]];
-      }); */
-      //var output = Object.entries(obj).map(([key, value]) => ({key,value}));
       var dataArray = Object.keys(obj).map(function(k){return obj[k]});
-
       return [
         dataArray
       ]
     };
-/*    
-    var result = [];
 
-result = data.map(function (el) {
-    var key = Object.keys(el).pop()
-
-    return [
-        key, +el[key]
-    ]
-});
-
-console.log(result);
-
-*/
     const myData = this.props.data;
     const myDataCount = myData.length;
     let myNumber = this.state.question;
+    
+    // get the data for the question we are on
     let myQuestion = findObjectByKey(myData,'menu_order',myNumber);
-    //var myQuestionArray = myQuestion.keys(obj).map((k) => obj[k])
 
-    //console.log( myQuestion );
-
-    let createMarkup = myhtml => {
-      return {__html: myhtml};
-    }
-    
-    let QSafeHTML = (myContent) => {
-      return <div dangerouslySetInnerHTML={createMarkup( myContent )} />;
-    }
-    
     if ( myQuestion) {
       var myQuestionArray = Object.entries(myQuestion);
 
       console.log(myQuestionArray);
 
+      // what kind of question are we? text/video/multiple choice/
       var QType = myQuestionArray[16][1]['type'];
 
       if (QContent) {
         console.log('QContent: ' + QContent);
-        //QSafeHTML();
-        console.log(QSafeHTML({QContent}));
       }
-
-   
-
 
       if (QType==='Text') {
         var QContent = myQuestionArray[16][1]['content'];
         var QRender = <div dangerouslySetInnerHTML={{ __html: QContent }}></div> 
-//      vimeo_code: PropTypes.string,
-//      video_intro_text: PropTypes.string.isRequired
-
       }
       if (QType==='Video') {
+        var video_intro_text = myQuestionArray[16][1]['video_intro_text'];
         var vimeo_code = myQuestionArray[16][1]['vimeo_code']
         var video_url = 'https://player.vimeo.com/video/' + vimeo_code + 'playing'
-
-        var video_intro_text = myQuestionArray[16][1]['video_intro_text'];
-        //console.log('v code: ' + vimeo_code);
-        //console.log('intro: ' + video_intro_text);
         
         var QRender = <div className='video-papa'>
         <div dangerouslySetInnerHTML={{ __html: video_intro_text }}></div>
@@ -113,18 +73,44 @@ console.log(result);
         var question_text = myQuestionArray[10][1]['rendered'];
         var options = myQuestionArray[16][1]['multiple_choice_question']; 
         
-        // create an array for the render list
+        console.log(options)
+        console.log('options: ', options, typeof options, Array.isArray(options));
+      //  var myOptionsArray = Object.entries(options);
+        var myOptionsArray = Object.entries(options);
+        console.log(myOptionsArray)
+        console.log('myOptionsArray: ', myOptionsArray, typeof myOptionsArray, Array.isArray(myOptionsArray));
+        console.log(myOptionsArray.indexOf('true'))
+        //console.log(options.entries(3))
+        //console.log(Object.entries(options));
+
+        //var index = options.indexOf('true'); 
+       // var index = myOptionsArray.findIndex(k => k===true);
+        //console.log(index);
+
+        //console.log('work mofo: ' + myOptionsArray.indexOf(true));
+
+        
+
+
+        //options.indexOf(searchElement[, fromIndex]);
+        // create an array for the question options list
         var rOptions = [];
         
         options.forEach(function(element) {
           rOptions.push(element.choice);
-          console.log(element.is_this_the_correct_answer)
-          // and a function to find who is right
-          function findCorrect() {
-            return element == true;
-          }
-          var correct = options.findIndex(findCorrect);
-          console.log(correct);
+          //console.log(element.is_this_the_correct_answer)
+
+        
+
+          
+          // and a function to find which one is right
+         
+
+          //var a = options.indexOf(true);
+          //if(arry[x].value === true){
+          //console.log('a: ' + a);
+          //var correct = options.findIndex(findCorrect);
+          //console.log(correct);
         });
         
         
