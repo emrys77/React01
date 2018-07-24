35401a0be42c9ffb458721eb71afa8c8bc6da65a
Emrys Hughes <>
2018-07-24 20:47:03 +1000

    re factor information box code

--- src/components/InformationBoxes/InformationBoxes.jsx
+++ src/components/InformationBoxes/InformationBoxes.jsx
@@ -1,56 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react'
 import PropTypes from 'prop-types';
-import ReactDOM from 'react-dom';
-import Modal from 'react-responsive-modal';
+import InformationModal from './InformationModal.jsx';
 
-export default class InformationBox extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {    open: false    };
-    };
-  
-    onOpenModal = () => {
-      this.setState({ open: true });
-    };
-  
-    onCloseModal = () => {
-      this.setState({ open: false });
-    };
-  
-    render() {
-        const { open } = this.state;
-        return (
-            <div>
-                <button onClick={this.onOpenModal}>Open modal</button>
-                <Modal open={open} onClose={this.onCloseModal} center>
-                    <h2>Simple centered modal</h2>
-                </Modal>
-            
-            <button onClick={this.onOpenModal}>Open modal 2</button>
-            <Modal open={open} onClose={this.onCloseModal} center>
-                <h2>Simple centered modal 2</h2>
-            </Modal>
-        </div>
-        );
-    }
-}
 
-/*  
-function InformationBoxes(props) {
-  return  (
+const InformationModals = (boxes) => {
+    return  (
         <div>
-            <div dangerouslySetInnerHTML={{ __html: props.intro }}></div>
-            <div dangerouslySetInnerHTML={{ __html: props.box1 }}></div>
-            <div dangerouslySetInnerHTML={{ __html: props.box2 }}></div>
+            {boxes.map((box, i) => <InformationModal obj={box} key={i} />)}
         </div>
-  );
-}
+    );
+  }
+  
+  InformationModals.propTypes = {
+    content: PropTypes.string.isRequired
+  };
+  
+  export default InformationModals;
 
-Text.propTypes = {
-  intro: PropTypes.string.isRequired,
-  box1: PropTypes.string.isRequired,
-  box2: PropTypes.string.isRequired
-};
-
-export default InformationBoxes;
-*/
+  
---
+++ src/components/InformationBoxes/InformationModal.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Modal from 'react-responsive-modal';
+
+export default class InformationModal extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {    open: false    };
+    };
+  
+    onOpenModal = () => {
+      this.setState({ open: true });
+    };
+  
+    onCloseModal = () => {
+      this.setState({ open: false });
+    };
+    // classNames
+  
+    render() {
+        const { open } = this.state;
+        return (
+            <div>
+                <button onClick={this.onOpenModal}>Open modal</button>
+                <Modal open={open} onClose={this.onCloseModal} center>
+                    <h2>Simple centered modal</h2>
+                </Modal>
+            </div>
+        );
+    }
+}
